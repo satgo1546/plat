23eebb24fcfb537ed149bbb21cd7cba278007326
Frog Chen <satgo1546@yeah.net>
2026-04-05 18:05:41 +0800

    Tying up Type Inference

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,72 @@
-import { test, expect } from 'vitest'
-import { infer } from './index.ts'
+import { test, expect, assert } from 'vitest'
+import { type Expression, infer, type Variable } from './index.ts'
 
-test('a test', () => {
-	expect(infer).toBeTruthy()
+function v(variableName: string): Variable {
+	return { tag: 'variable', name: variableName }
+}
+
+function n(value: number): Expression & { tag: 'number' } {
+	return { tag: 'number', value }
+}
+
+function λ(parameterName: string, body: Expression): Expression & { tag: 'function' } {
+	return { tag: 'function', parameter: v(parameterName), body }
+}
+
+function $(callee: Expression, argument: Expression): Expression & { tag: 'call' } {
+	return { tag: 'call', callee, argument }
+}
+
+test('infers_int', () => {
+	const expr = n(114514)
+	expect(infer(expr)).toStrictEqual({ tag: 'number' })
+	expect(expr).toStrictEqual(n(114514))
+})
+
+test('infers_id_fun', () => {
+	const expr = λ('x', v('x'))
+	const type = infer(expr)
+	assert(type.tag === 'function')
+	expect(type.parameter).toBe(type.returnType)
+	expect(type.parameter.tag).toBe('typeVariable')
+})
+
+test('infers_k_combinator', () => {
+	const expr = λ('x', λ('y', v('x')))
+	const type = infer(expr)
+	assert(type.tag === 'function')
+	assert(type.returnType.tag === 'function')
+	expect(type.parameter).toBe(type.returnType.returnType)
+	expect(type.parameter.tag).toBe('typeVariable')
+	expect(type.returnType.parameter.tag).toBe('typeVariable')
+})
+
+test('infers_s_combinator', () => {
+	const expr = λ('x', λ('y', λ('z', $($(v('x'), v('z')), $(v('y'), v('z'))))))
+	const type = infer(expr)
+	assert(type.tag === 'function')
+	assert(type.parameter.tag === 'function')
+	assert(type.parameter.parameter.tag === 'typeVariable')
+	assert(type.parameter.returnType.tag === 'function')
+	assert(type.parameter.returnType.parameter.tag === 'typeVariable')
+	assert(type.parameter.returnType.returnType.tag === 'typeVariable')
+	assert(type.returnType.tag === 'function')
+	assert(type.returnType.parameter.tag === 'function')
+	assert(type.returnType.parameter.parameter.tag === 'typeVariable')
+	assert(type.returnType.parameter.returnType.tag === 'typeVariable')
+	assert(type.returnType.returnType.tag === 'function')
+	assert(type.returnType.returnType.parameter.tag === 'typeVariable')
+	assert(type.returnType.returnType.returnType.tag === 'typeVariable')
+	expect(type.parameter.parameter).toBe(type.returnType.parameter.parameter)
+	expect(type.parameter.parameter).toBe(type.returnType.returnType.parameter)
+	expect(type.parameter.parameter).not.toBe(type.parameter.returnType.parameter)
+	expect(type.parameter.parameter).not.toBe(type.returnType.returnType.returnType)
+	expect(type.parameter.returnType.parameter).toBe(type.returnType.parameter.returnType)
+	expect(type.returnType.returnType.returnType).toBe(type.parameter.returnType.returnType)
+	expect(type.returnType.returnType.returnType).not.toBe(type.parameter.returnType.parameter)
+})
+
+test('type_infer_fails', () => {
+	const expr = $(λ('x', $(v('x'), n(3))), n(1))
+	expect(() => infer(expr)).toThrow(expect.objectContaining({ tag: 'typeMismatch' }))
 })
--- index.ts
+++ index.ts
@@ -6,7 +6,11 @@ export type Expression =
 	| { tag: 'function', parameter: Variable, body: Expression }
 	| { tag: 'call', callee: Expression, argument: Expression }
 
-export type TypeVariable = { tag: 'typeVariable', name: string }
+export type TypeVariable = {
+	tag: 'typeVariable',
+	link?: Type,
+	name: string,
+}
 
 export type Type =
 	| TypeVariable
@@ -21,10 +25,45 @@ export function functionType(...types: Type[]): Type {
 	}))
 }
 
+function find(type: Type): Type {
+	switch (type.tag) {
+		case 'typeVariable':
+			if (type.link) {
+				return type.link = find(type.link)
+			}
+			break
+		case 'function':
+			type.parameter = find(type.parameter)
+			type.returnType = find(type.returnType)
+			break
+	}
+	return type
+}
+
+function substitute_expression(expression: Expression) {
+	switch (expression.tag) {
+		case 'variable':
+			if (expression.type) expression.type = find(expression.type)
+			break
+		case 'function':
+			if (expression.parameter.type) expression.parameter.type = find(expression.parameter.type)
+			substitute_expression(expression.body)
+			break
+		case 'call':
+			substitute_expression(expression.callee)
+			substitute_expression(expression.argument)
+			break
+	}
+}
+
 export type Scope = Record<string, Type>
 
 type Constraint =
 	| { tag: '=', 0: Type, 1: Type }
+
+type TypeError =
+	| { tag: 'typeMismatch', 0: Type, 1: Type }
+	| { tag: 'infiniteType', typeVariable: TypeVariable, type: Type }
 
 class Inferrer {
 	constraints: Constraint[] = []
@@ -73,6 +112,48 @@ class Inferrer {
 			this.constraints.push({ tag: '=', 0: type, 1: actualType })
 		}
 	}
+
+	solve(): TypeError | undefined {
+		for (const constraint of this.constraints) switch (constraint.tag) {
+			case '=': {
+				const error = this.unify(constraint[0], constraint[1])
+				if (error) return error
+				break
+			}
+		}
+	}
+
+	unify(a: Type, b: Type): TypeError | undefined {
+		a = find(a)
+		b = find(b)
+		if (a.tag === 'number' && b.tag === 'number') {
+			// nothing to do
+		} else if (a.tag === 'function' && b.tag === 'function') {
+			return this.unify(a.parameter, b.parameter)
+				?? this.unify(a.returnType, b.returnType)
+		} else if (a.tag === 'typeVariable' && b.tag === 'typeVariable') {
+			if (a.name === b.name) return
+			a.link = b
+		} else if (a.tag === 'typeVariable') {
+			const occurs = (function recurse(type: Type): boolean {
+				type = find(type)
+				switch (type.tag) {
+					case 'typeVariable':
+						return type.name === a.name
+					case 'number':
+						return false
+					case 'function':
+						return recurse(type.parameter) || recurse(type.returnType)
+				}
+			})(b)
+			if (occurs) return { tag: 'infiniteType', typeVariable: a, type: b }
+			a.link = b
+		} else if (b.tag === 'typeVariable') {
+			this.unify(b, a)
+		} else {
+			return { tag: 'typeMismatch', 0: a, 1: b }
+		}
+	}
 }
 
 /**
@@ -86,5 +167,8 @@ export function infer(expression: Expression): Type {
 	for (const constraint of inferrer.constraints) {
 		console.log('•', constraint)
 	}
-	return type
+	const error = inferrer.solve()
+	if (error) throw error
+	substitute_expression(expression)
+	return find(type)
 }
