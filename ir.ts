a7811e8119759d25522b69330308c35eca9b0e37
Frog Chen <satgo1546@yeah.net>
2026-04-07 16:04:03 +0800

    Lowering our AST to Escape the Typechecker

---
+++ ir.ts
@@ -0,0 +1,65 @@
+import { isDeepStrictEqual } from 'node:util'
+import { Expression } from './index.ts'
+
+export type Variable = { tag: 'variable', name: string, type: Type }
+
+export type Node =
+	| Variable
+	| { tag: 'number', value: number }
+	| { tag: 'function', parameter: Variable, body: Node }
+	| { tag: 'call', callee: Node, argument: Node }
+	| { tag: 'let', variable: Variable, value: Node, body: Node }
+
+export type Type =
+	| { tag: 'typeVariable', index: number }
+	| { tag: 'number' }
+	| { tag: 'function', parameter: Type, returnType: Type }
+	| { tag: 'generic', parameter: Kind, definition: Type }
+
+export type Kind = 'type'
+
+function reify(type: Type, argument: Type, index = 0): Type {
+	switch (type.tag) {
+		case 'typeVariable':
+			if (type.index === index) return argument
+			if (type.index < index) return type
+			return { tag: 'typeVariable', index: type.index - 1 }
+		case 'number':
+			return type
+		case 'function':
+			return {
+				tag: 'function',
+				parameter: reify(type.parameter, argument),
+				returnType: reify(type.returnType, argument),
+			}
+		case 'generic':
+			return {
+				tag: 'generic',
+				parameter: type.parameter,
+				definition: reify(type.definition, argument, index + 1),
+			}
+	}
+}
+
+function getNodeType(node: Node): Type {
+	switch (node.tag) {
+		case 'variable':
+			return node.type
+		case 'number':
+			return { tag: 'number' }
+		case 'function':
+			return { tag: 'function', parameter: node.parameter.type, returnType: getNodeType(node.body) }
+		case 'call':
+			const calleeType = getNodeType(node.callee)
+			if (calleeType.tag !== 'function') throw new Error('impossible')
+			if (!isDeepStrictEqual(getNodeType(node.argument), calleeType.parameter)) throw new Error('impossible')
+			return calleeType.returnType
+		case 'let':
+			if (!isDeepStrictEqual(node.variable.type, getNodeType(node.value))) throw new Error('impossible')
+			return getNodeType(node.body)
+	}
+}
+
+export function lower(expression: Expression, type: Type) {
+	throw Error('not implemented')
+}
